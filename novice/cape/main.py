1b36d599243f633eeb77bd333f6c37158894ea3b
khalila2504 <khalilafathiya04@gmail.com>
2022-09-14 23:11:28 +0700

    update

--- novice/cape/main.py
+++ novice/cape/main.py
@@ -4,81 +4,77 @@ import psycopg2
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
-def cape():
+def hewan():
 
     conn = psycopg2.connect(
         host="localhost",
-        database="cape",
+        database="hewan",
         user="postgres",
         password="hda182526"
     )
     curs = conn.cursor()
     if request.method == "POST":
         nama = request.form.get("nama")
-        actions = request.form.get("actions")
-        query = f"insert into cape(nama, actions) values('{nama}', '{actions}')"
+        kingdom = request.form.get("kingdom")
+        filum = request.form.get("filum")
+        kelas = request.form.get("kelas")
+        ordo = request.form.get("ordo")
+        famili = request.form.get("famili")
+        genus = request.form.get("genus")
+        spesies = request.form.get("spesies")
+        query = f"insert into taksonomi(nama, kingdom, filum, kelas, ordo, famili, genus, spesies) values('{nama}', '{kingdom}', '{filum}', '{kelas}', '{ordo}', '{famili}', '{genus}', '{spesies}')"
         curs.execute(query)
         conn.commit()
 
     print(request.method)
-    query = f"select * from cape order by id desc"
+    query = f"select * from taksonomi order by id desc"
     curs.execute(query)
     data = curs.fetchall()
     curs.close()
     conn.close()
     return render_template("cape.html", context=data)
     
-@app.route("/detail/<cape_id>") 
-def detail(cape_id):
-    conn = psycopg2.connect(
-        host="localhost",
-        database="cape",
-        user="postgres",
-        password="hda182526"
-    )
-    curs = conn.cursor()
-    query = f"select * from cape where id = {cape_id}"
-    curs.execute(query)
-    data = curs.fetchone()
-    curs.close()
-    conn.close()
-    print(data)
-    return render_template("detail.html", context=data)
 
-@app.route("/delete/<cape_id>")
-def delete(cape_id):
+@app.route("/delete/<taksonomi_id>")
+def delete(taksonomi_id):
     conn = psycopg2.connect(
         host="localhost",
-        database="cape",
+        database="hewan",
         user="postgres",
         password="hda182526"
     )
     curs = conn.cursor()
-    query = f"delete from cape where id = {cape_id}"
+    query = f"delete from taksonomi where id = {taksonomi_id}"
     curs.execute(query)
     conn.commit()
     curs.close()
     conn.close()
     return redirect("/")
 
-@app.route("/update/<cape_id>", methods=["GET", "POST"])
-def update(cape_id):
+@app.route("/update/<taksonomi_id>", methods=["GET", "POST"])
+def update(taksonomi_id):
     conn = psycopg2.connect(
         host="localhost",
-        database="cape",
+        database="hewan",
         user="postgres",
         password="hda182526"
     )
     curs = conn.cursor()
     if request.method == "POST":
         nama = request.form.get("nama")
-        actions = request.form.get("actions")
-        query = f"update cape set nama ='{nama}', actions = '{masalah}' where id = {cape_id}"
+        kingdom = request.form.get("kingdom")
+        filum = request.form.get("filum")
+        kelas = request.form.get("kelas")
+        ordo = request.form.get("ordo")
+        famili = request.form.get("famili")
+        genus = request.form.get("genus")
+        spesies = request.form.get("spesies")
+        query = f"update taksonomi set nama = '{nama}', kingdom = '{kingdom}', filum = '{filum}', kelas = '{kelas}', ordo = '{ordo}', famili = '{famili}', genus = '{genus}', spesies = '{spesies}') values('{nama}', '{kingdom}', '{filum}', '{kelas}', '{ordo}', '{famili}', '{genus}', '{spesies}')"
         curs.execute(query)
         conn.commit()
-        return redirect("/")
-    
-    query = f"select * from cape where id = {cape_id}"
+        return redirect ("/")
+ 
+    query = f"select * from taksonomi where id = {taksonomi_id}"
     curs.execute(query)
     data = curs.fetchone()
     # conn.commit()
